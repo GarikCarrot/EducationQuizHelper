516e3ae3f1060f7b10d01cee32634e21dbf29b67
garik-carrot <harry-123@yandex.ru>
2018-06-03 20:29:00 +0300

    Add sync

--- app/src/main/java/com/garikcarrot/education/MainActivity.kt
+++ app/src/main/java/com/garikcarrot/education/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        FirebaseDatabase.getInstance().reference.keepSynced(true)
         supportFragmentManager.beginTransaction()
                 .add(R.id.container, ChooseSubjectFragment())
                 .commit()
